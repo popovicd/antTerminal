b80e516cec00416aca5ea3bcea5cfa443de92dcd
Davor Popovic <davor.popeye@gmail.com>
2022-08-05 14:13:50 +0200

    feat(terminal.c terminal.h): add raw terminal support
    Signed-off-by: Davor Popovic <davor.popeye@gmail.com>

---
+++ terminal.c
@@ -0,0 +1,57 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/ioctl.h>
+#include <termios.h>
+#include <unistd.h>
+
+#include "terminal.h"
+
+struct termios orig_termios;
+
+void terminal_error(int error) {
+	switch(error) {
+		case TCSETATTR:
+			perror("Error at setting terminal attributes: ");
+			exit(1);
+		case TCGETATTR:
+			perror("Error at getting terminal attributes: ");
+			exit(1);
+		deafult:
+			exit(1);
+	}
+}
+
+void terminal_disable_raw() {
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
+int terminal_enable_raw() {
+    if (tcgetattr(STDIN_FILENO, &orig_termios) == -1)
+        return -1;
+
+	/* Set the new attributes for the terminal */
+    struct termios raw_terminal;
+
+	raw_terminal = orig_termios;
+
+	raw_terminal.c_iflag &= ~(ICRNL | IXON);
+    raw_terminal.c_oflag &= ~(OPOST);
+    raw_terminal.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+    raw_terminal.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+    raw_terminal.c_cflag |= (CS8);
+    raw_terminal.c_cc[VMIN] = 0;
+    raw_terminal.c_cc[VTIME] = 1;
+
+	/* on exit, restore the original terminal */
+	atexit(terminal_disable_raw);
+
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw_terminal) == -1)
+        return -1;
+
+    return 0;
+}
+
+
---
+++ terminal.h
@@ -0,0 +1,19 @@
+#ifndef __TERMINAL_H__
+#define __TERMINAL_H__
+
+/*
+struct terminal_context term_ctx {
+
+}
+*/
+
+enum error {
+	TCSETATTR = 0,
+	TCGETATTR,
+};
+
+int terminal_enable_raw();
+void terminal_disable_raw();
+void terminal_error(int error);
+
+#endif /* __TERMINAL_H__ */
