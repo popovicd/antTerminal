ace8247726b23e00b36c8ff89df4877356ba48e9
Davor Popovic <davor.popeye@gmail.com>
2022-08-08 19:44:49 +0200

    feat(shell.c shell.h): add line arguments parsing
    Signed-off-by: Davor Popovic <davor.popeye@gmail.com>

--- shell.c
+++ shell.c
@@ -12,16 +12,41 @@
 
 extern struct terminal_context t_ctx;
 
-void shell_getline(char *line) {
+int shell_getline(char *line) {
 	int i;
 	terminal_init();
 
-	while(1) {
-		terminal_prompt(PROMPT);
-		terminal_getline(line);
+	terminal_prompt(PROMPT);
+	int rval = terminal_getline(line);
 
-		terminal_putstring(line, strlen(line));
+	terminal_putstring(line, strlen(line));
 
-		terminal_putstring("\r\n", 2);
-	}
+	terminal_putstring("\r\n", 2);
+
+	return rval;
 }
+
+int shell_parseline(char ***argv, int *argc, char *line) {
+    char *token;
+    int argument_count;
+
+    (*argv) = malloc(sizeof(char *) * MAX_ARGV);
+    if((*argv) == NULL) {
+        return SHELL_NO_MEMORY;
+    }
+
+    for(argument_count = 0, token = strtok(line, " "); token ;
+		token = strtok(0, " "), argument_count++) {
+
+ 	   	(*argv)[argument_count] = malloc(sizeof(char) * strlen(token) + 1);
+        if((*argv)[argument_count] == NULL) {
+            return SHELL_NO_MEMORY;
+        }
+        strcpy((*argv)[argument_count], token);
+    }
+
+    *argc = argument_count;
+    return SHELL_OK;
+}
+
+
--- shell.h
+++ shell.h
@@ -1,6 +1,15 @@
 #ifndef __SHELL_H__
 #define __SHELL_H__
 
-void shell_getline(char *line);
+#define MAX_ARGV 	5
+
+enum shell_status {
+    SHELL_OK = 0,
+    SHELL_NO_MEMORY = -1,
+    SHELL_COMMAND_NOT_FOUND = -2,
+};
+
+int shell_getline(char *line);
+int shell_parseline(char ***argv, int *argc, char *line);
 
 #endif /* __SHELL_H__ */
