133f7ee3702af990ad2f55a8190af85456c89179
Davor Popovic <davor.popeye@gmail.com>
2022-08-08 20:14:15 +0200

    feat(shell.c): add command execution handling
    Signed-off-by: Davor Popovic <davor.popeye@gmail.com>

--- shell.c
+++ shell.c
@@ -12,12 +12,25 @@
 
 extern struct terminal_context t_ctx;
 
+char *shell_commands[] = {
+	"cmd-example-1",
+	"cmd-example-2",
+	"cmd-example-3",
+	"cmd-exit"
+};
+
+int (*command_functions[]) (char **argv, int argc) = {
+	&shell_cmd_ex_one,
+	&shell_cmd_ex_two,
+	&shell_cmd_ex_three
+};
+
 int shell_getline(char *line) {
-	int i;
+	int rval, i;
 	terminal_init();
 
 	terminal_prompt(PROMPT);
-	int rval = terminal_getline(line);
+	rval = terminal_getline(line);
 
 	terminal_putstring(line, strlen(line));
 
@@ -49,4 +62,15 @@ int shell_parseline(char ***argv, int *argc, char *line) {
     return SHELL_OK;
 }
 
+int shell_executecmd(char **argv, int argc) {
+	int rval, i;
 
+    for (i = 0; i < sizeof(shell_commands) / sizeof(char *); i++) {
+        if (strcmp(argv[0], shell_commands[i]) == 0) {
+            rval = (*command_functions[i])(argv, argc);
+            return rval;
+        }
+    }
+
+    return SHELL_COMMAND_NOT_FOUND;
+}
